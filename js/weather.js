6d50b6cb03ed2bfa6881a6afba8e912e628d4927
Amarjot Parmar <amarjot.parmar@protonmail.com>
2020-10-06 21:03:20 +1300

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = 'b3c689e558a2eec385052a40ca78f50d';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = -41.286461;
+    let longitude = 174.776230;
 
     getWeather(latitude, longitude);
 }
